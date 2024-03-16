62259319aa76b07e4daf117ef0fb5858e2686f3a
Syed Ali Hashmi <160578567+alihashmi2288@users.noreply.github.com>
2024-03-16 14:25:32 +0500

    Update app.ts

--- Exercise_45/app.ts
+++ Exercise_45/app.ts
@@ -1,3 +1,19 @@
-// skip
+//EXercise 45:
+// Cars: 
+
+function car(manufacturer: string, model: string,...args: any) {
+  let car: any = {
+    manufacturer: manufacturer,
+    model: model
+  };
+  for (let i = 0; i < args.length; i++) {
+    car[args[i][0]] = args[i+1];
+  }
+  return car;
+}
+
+console.log(car("Ford", "Mustang", ["color", "red"], ["year", 1969]));
+
+//for this code i take help of AI to understand
 
 

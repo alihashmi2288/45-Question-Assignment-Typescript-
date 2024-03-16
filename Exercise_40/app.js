293284bece4234925f03b52f54af18d8e9023473
Syed Ali Hashmi <160578567+alihashmi2288@users.noreply.github.com>
2024-03-16 13:52:35 +0500

    Update app.js

--- Exercise_40/app.js
+++ Exercise_40/app.js
@@ -1,4 +1,17 @@
 "use strict";
 // Exercise :40
 // Album:
-//can't understand
+
+function make_album(artist, title, num_tracks) {
+    return {
+        artist: artist,
+        title: title,
+        num_tracks: num_tracks
+    };
+}
+let album1 = make_album("Pink Floyd", "Dark Side of the Moon", 17);
+console.log(album1);
+let album2 = make_album("Pink Floyd", "The Wall");
+console.log(album2);
+let album3 = make_album("Pink Floyd", "The Wall", 17);
+console.log(album3);

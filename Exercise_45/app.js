c4b90532888f42ee3cb7650dd3cb6a561ed89880
Syed Ali Hashmi <160578567+alihashmi2288@users.noreply.github.com>
2024-03-16 14:26:19 +0500

    Update app.js

--- Exercise_45/app.js
+++ Exercise_45/app.js
@@ -1,2 +1,18 @@
 "use strict";
-// skip
+//Exercise 45:
+// Cars: 
+
+"use strict";
+function car(manufacturer, model, ...args) {
+    let car = {
+        manufacturer: manufacturer,
+        model: model
+    };
+    for (let i = 0; i < args.length; i++) {
+        car[args[i][0]] = args[i + 1];
+    }
+    return car;
+}
+console.log(car("Ford", "Mustang", ["color", "red"], ["year", 1969]));
+//for this code i take help of AI to understand
+
